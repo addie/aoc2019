3ac682a958020fff3ac95f6f2c3a4b27dc968c46
Addie Bendory <addie.bendory@uber.com>
2019-12-03 23:55:23 -0500

    Day 3

---
+++ day4.py
@@ -0,0 +1,12 @@
+from typing import List, AnyStr, Tuple, Set
+
+
+def part1(w1: List[AnyStr], w2: List[AnyStr]) -> int:
+    pass
+
+
+def part2(w1: List[AnyStr], w2: List[AnyStr]) -> int:
+    pass
+
+
+if __name__ == '__main__':
